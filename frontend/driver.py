51f56788bd317c25ebf2158ad5477995497be592
Chun Yip Wong <cy.jimmy.wong@gmail.com>
2023-02-27 20:34:09 -0800

    Implement function to create empty squares

--- frontend/driver.py
+++ frontend/driver.py
@@ -1,4 +1,3 @@
-import random
 import sys
 from PySide6 import QtWidgets
 
@@ -17,46 +16,14 @@ class GeneratePuzzleWindow(QtWidgets.QMainWindow, generatepuzzle.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        # self.pushButton_3.clicked.connect(self.generate_sudoku)
-        self.pushButton_3.clicked.connect(self.improved_generate_sudoku)
-
-    @staticmethod
-    def improved_generate_sudoku():
-        generator = SudokuGenerator(16)
-        board = generator.generate()
-        for row in board:
-            print(row)
-        print("---------------")
+        self.pushButton_3.clicked.connect(self.generate_sudoku)
 
     @staticmethod
     def generate_sudoku():
-        size = 9
-        values = set(range(1, size + 1))
-        subgrid_row = 3
-        subgrid_col = 3
-        sudoku_grid = [[0] * size for _ in range(size)]
-        valid_board_generated = False
-        while not valid_board_generated:
-            try:
-                for row_index, row in enumerate(sudoku_grid):
-                    subgrid_start_row_index = (row_index // subgrid_row) * subgrid_row
-                    subgrid_row_indices = range(subgrid_start_row_index, subgrid_start_row_index + subgrid_row)
-                    for col_index, col in enumerate(row):
-                        subgrid_start_col_index = (col_index // subgrid_col) * subgrid_col
-                        subgrid_col_indices = range(subgrid_start_col_index, subgrid_start_col_index + subgrid_col)
-                        grid_values = set([sudoku_grid[r][c] for r in subgrid_row_indices for c in subgrid_col_indices])
-                        row_values = set(cell for cell in row)
-                        col_values = set([r[col_index] for r in sudoku_grid])
-                        used_values = grid_values | row_values | col_values
-                        unused_values = list(values - used_values)
-                        sudoku_grid[row_index][col_index] = random.choice(unused_values)
-                valid_board_generated = True
-            except IndexError:
-                sudoku_grid = [[0] * size for _ in range(size)]
-        for row in sudoku_grid:
-            for col in row:
-                print(col, end="")
-            print("")
+        generator = SudokuGenerator(25)
+        board = generator.generate()
+        for row in board:
+            print(row)
         print("---------------")
 
 
--- frontend/sudoku_generator.py
+++ frontend/sudoku_generator.py
@@ -9,10 +9,15 @@ class SudokuGenerator:
         self._block_cols = size // self._block_rows
         self._board = [[0] * size for _ in range(size)]
         self._values = range(1, size + 1)
+        self._empty = 0.75
 
     def generate(self):
-        self._fill_diagonal_block()
-        self._fill_cell(0, 0)
+        if self._size <= 16:
+            self._fill_diagonal_block()
+            self._fill_cell(0, 0)
+        else:
+            self._generate_from_file()
+        self._remove_cell_values()
         return self._board
 
     def _fill_diagonal_block(self):
@@ -58,3 +63,22 @@ class SudokuGenerator:
                 if self._board[r][c] == value:
                     return False
         return True
+
+    def _generate_from_file(self):
+        file_number = random.choice([1, 2, 3])
+        file = f'{self._size}-{file_number}.txt'
+        with open(file) as input_file:
+            rows = input_file.readlines()
+            for index, row in enumerate(rows):
+                self._board[index] = [int(value) for value in row.split()]
+
+    def _remove_cell_values(self):
+        total_cell = self._size ** 2
+        filled_cell = math.floor(total_cell * (1 - self._empty))
+        board = [[0] * self._size for _ in range(self._size)]
+        random_index = random.sample(range(total_cell), filled_cell)
+        for index in random_index:
+            row = index // self._size
+            col = index % self._size
+            board[row][col] = self._board[row][col]
+        self._board = board
