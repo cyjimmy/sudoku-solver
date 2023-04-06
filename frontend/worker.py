e722d802d0b4aeeaffcfc2e495f92f8634a469ac
Miniature-Pug <msrandhawa9957@gmail.com>
2023-04-06 05:21:14 -0700

    added Multiprocessing to threaded workers

--- frontend/worker.py
+++ frontend/worker.py
@@ -1,7 +1,8 @@
 import sys
+import traceback
+from multiprocessing import Process, Queue
 
 from PySide6.QtCore import QRunnable, Slot, QObject, Signal
-import traceback
 
 
 class Worker(QRunnable):
@@ -15,6 +16,7 @@ class Worker(QRunnable):
         self.__result = None
         self.__finished_status = None
         self.__timer = None
+        self.queue = Queue()
 
     @property
     def finished_status(self):
@@ -29,10 +31,18 @@ class Worker(QRunnable):
     def timer(self):
         return self.__timer
 
+    def __helper(self, queue, fn):
+        ret = fn()
+        queue.put(ret)
+
     @Slot()
     def run(self):
         try:
-            self.__result, self.__timer = self.fn(*self.args, **self.kwargs)
+            args = [self.queue, self.fn]
+            p = Process(target=self.__helper, args=args)
+            p.start()
+            self.__result, self.__timer = self.queue.get()
+            p.join()
         except:
             traceback.print_exc()
             exctype, value = sys.exc_info()[:2]
